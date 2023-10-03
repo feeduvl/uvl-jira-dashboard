418136e898f598e19ec8a34b0609cdeb18ff4cfd
tunao <benjamintuna@gmail.com>
2023-10-03 17:19:35 +0200

    adjustments

---
+++ controller/feedback.py
@@ -0,0 +1,167 @@
+import os
+# from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+# import torch
+import requests
+from flask import Flask, request, jsonify, Blueprint
+from pymongo import MongoClient
+
+feedback_bp = Blueprint('feedback', __name__)
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["jira-issues"]
+collectionJiraIssues = db["jiraIssue"]
+collectionFeedback = db["feedback"]
+collectionAnnotations = db["annotations"]
+collectionFeedbackWithToreCategories = db["feedback_wth_tore"]
+
+
+@feedback_bp.route('/assign_feedback_to_issues', methods=['POST'])
+def assign_feedback_to_issues():
+    return
+
+
+@feedback_bp.route('/assign_feedback_to_issues_by_tore', methods=['POST'])
+def assign_feedback_to_issues_by_tore():
+    feedback_collection = list(collectionFeedbackWithToreCategories.find({}))
+    jira_issues = collectionJiraIssues.find({})
+    assigned_feedback = []
+    for issue in jira_issues:
+        issue_type = issue.get('issueType')
+        for feedback in feedback_collection:
+            assigned_tore = feedback.get('tore', [])
+            for tore in assigned_tore:
+                if issue_type == tore:
+                    id_and_text = {
+                        'id': feedback.get('id'),
+                        'text': feedback.get('text')
+                    }
+                    assigned_feedback.append(id_and_text)
+        if assigned_feedback:
+            issue_id = issue.get('_id')
+            update_criteria = {"_id": issue_id}
+            update_operation = {"$set": {"feedback": assigned_feedback}}
+            collectionJiraIssues.update_one(update_criteria, update_operation)
+            assigned_feedback = []
+    issues_new = list(collectionJiraIssues.find({}))
+    for element in issues_new:
+        element["_id"] = str(element["_id"])
+    return issues_new
+
+
+@feedback_bp.route('/assign_tore_to_feedback/<annotation_name>', methods=['GET'])
+def set_tore_categories(annotation_name):
+    try:
+        feedback_collection = collectionFeedbackWithToreCategories.find({})
+        docs = collectionAnnotations.distinct('docs')
+        codes = collectionAnnotations.distinct('codes')
+        tore_list = []
+
+        for feedback in feedback_collection:
+            document_id = feedback.get('id')
+            for doc in docs:
+                doc_name = doc.get('name')
+                if document_id == doc_name:
+                    begin_index = doc.get('begin_index')
+                    end_index = doc.get('end_index')
+                    for code in codes:
+                        tokens = code.get('tokens', [])
+                        for token in tokens:
+                            if begin_index <= token <= end_index:
+                                tore = code.get('tore')
+                                if tore not in tore_list:
+                                    tore_list.append(tore)
+
+                    if tore_list:
+                        feedback_id = feedback.get('_id')
+                        update_criteria = {"_id": feedback_id}
+                        update_operation = {"$set": {"tore": set_issue_type_by_tore_category(tore_list)}}
+                        collectionFeedbackWithToreCategories.update_one(update_criteria, update_operation)
+                        tore_list.clear()
+        feedback_new = list(collectionFeedbackWithToreCategories.find({}))
+        for element in feedback_new:
+            element["_id"] = str(element["_id"])
+        return feedback_new
+    except Exception as e:
+        return jsonify({"message": str(e)})
+
+
+def set_issue_type_by_tore_category(tore_list):
+    issue_types = []
+    for category in tore_list:
+        if category == "Task":
+            issue_types.append("User Task")
+        elif category == "Activity":
+            issue_types.append("User Subtask")  # ?
+            issue_types.append("User Story")
+        elif category == "Domain Data":
+            issue_types.append("Domain Data Diagram")  # Diagram?
+            issue_types.append("User Task")
+            issue_types.append("User Subtask")  # User Sub-Task?
+        elif category == "Stakeholder":
+            issue_types.append("Persona")
+            issue_types.append("User Role")
+            issue_types.append("User Story")
+        elif category == "Interaction":
+            issue_types.append("System Function")
+            issue_types.append("UI-Structure Diagram")  # ?
+        elif category == "Interaction Data":
+            issue_types.append("System Function")
+            issue_types.append("Workspace")
+        elif category == "Workspace":
+            issue_types.append("Workspace")
+            issue_types.append("UI-Structure Diagram")  # ?
+        elif category == "System Function":
+            issue_types.append("System Function")
+    types_set = set(issue_types)
+    types_list = list(types_set)
+    return types_list
+
+
+@feedback_bp.route("/load/<feedback_name>", methods=["GET"])
+def load_feedback(feedback_name):
+    try:
+        if collectionFeedbackWithToreCategories.count_documents({}) > 0:
+            collectionFeedbackWithToreCategories.delete_many({})
+
+        feedback_collection = collectionFeedback.find({})
+
+        for feedback in feedback_collection:
+            documents = feedback.get('documents', [])
+
+            for doc in documents:
+                doc_id = doc.get('id')
+                doc_text = doc.get('text')
+
+                id_and_text = {
+                    'id': doc_id,
+                    'text': doc_text
+                }
+                collectionFeedbackWithToreCategories.insert_one(id_and_text)
+        feedback_new = list(collectionFeedbackWithToreCategories.find({}))
+        for element in feedback_new:
+            element["_id"] = str(element["_id"])
+        return jsonify(feedback_new)
+    except Exception as e:
+        return jsonify({"message": str(e)})
+
+
+@feedback_bp.route('/get_feedback', methods=['GET'])
+def get_feedback():
+    issues = list(collectionFeedbackWithToreCategories.find({}))
+    for element in issues:
+        element["_id"] = str(element["_id"])
+    return issues
+
+
+@feedback_bp.route('/get_feedback_names', methods=['GET'])
+def get_feedback_names():
+    feedback = collectionFeedback.find({})
+    names_list = [doc["name"] for doc in feedback]
+    return names_list
+
+
+@feedback_bp.route('/get_annotations_names', methods=['GET'])
+def get_annotations_names():
+    annotations = collectionAnnotations.find({})
+    names_list = [doc["name"] for doc in annotations]
+    return names_list
---
+++ controller/jira.py
@@ -0,0 +1,182 @@
+from flask import Blueprint
+import os
+# from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+# import torch
+import requests
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+
+jira_issue_bp = Blueprint('jira_issue', __name__)
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["jira-issues"]
+collectionJiraProjects = db["jiraProject"]
+collectionJiraIssues = db["jiraIssue"]
+
+
+@jira_issue_bp.route("/load/issues/<project_name>", methods=["POST"])
+def load_issues_from_project(project_name):
+    data = request.json
+    list = []
+
+    issue_types = [item["item"] for item in data["jsonObject"]]
+
+    for issue_type in issue_types:
+        uri = f"https://jira-se.ifi.uni-heidelberg.de/rest/api/2/search?jql=project={project_name} AND issuetype='{issue_type}'&maxResults=520"
+        response = requests.get(
+            uri,
+            auth=(os.environ['username'], os.environ['password']),
+            headers={"Accept": "application/json"}
+        )
+
+        response_json = response.json()
+        total_issues = int(response_json.get("total", 0))
+
+        for i in range(total_issues):
+            issue_key = response_json["issues"][i]["key"]
+            issue_type = response_json["issues"][i]["fields"]["issuetype"]["name"]
+            project_name = response_json["issues"][i]["fields"]["project"]["name"]
+            summary = response_json["issues"][i]["fields"]["summary"]
+            issue = {"key": issue_key, "issueType": issue_type, "projectName": project_name, "summary": summary}
+            list.append(issue)
+
+    return jsonify(list)
+
+
+@jira_issue_bp.route("/import", methods=["POST"])
+def import_jira_issues():
+    data = request.json
+    collectionJiraIssues.delete_many({})
+    saved_issues = []
+
+    for item in data["jsonObject"]:
+        key = item["key"]
+        project_name = item["projectName"]
+        issue_type = item["issueType"]
+        summary = item["summary"]
+        jira_issue = {
+            "key": key,
+            "issueType": issue_type,
+            "projectName": project_name,
+            "summary": summary
+        }
+        saved_issue = collectionJiraIssues.insert_one(jira_issue)
+        saved_issue_info = {
+            "inserted_id": str(saved_issue.inserted_id),
+            "key": key,
+            "issueType": issue_type,
+            "projectName": project_name,
+            "summary": summary
+        }
+        saved_issues.append(saved_issue_info)
+
+    return jsonify(saved_issues)
+
+
+@jira_issue_bp.route("/add", methods=["POST"])
+def add_jira_issues():
+    data = request.json
+    saved_issues = list(collectionJiraIssues.find({}))
+
+    for item in data["jsonObject"]:
+        key = item["key"]
+        project_name = item["projectName"]
+        issue_type = item["issueType"]
+        summary = item["summary"]
+        jira_issue = {
+            "key": key,
+            "issueType": issue_type,
+            "projectName": project_name,
+            "summary": summary
+        }
+        already_used = any(jira_issue["key"] == key for jira_issue in saved_issues)
+        if not already_used:
+            collectionJiraIssues.insert_one(jira_issue)
+
+    updated_issues = list(collectionJiraIssues.find({}))
+    for element in updated_issues:
+        element["_id"] = str(element["_id"])
+
+    return jsonify(updated_issues)
+
+
+@jira_issue_bp.route("/all", methods=["GET"])
+def get_all_jira_issues_from_db():
+    try:
+        page = int(request.args.get("page", default=1))
+        size = int(request.args.get("size", default=-1))
+
+        if size == -1:
+            size = collectionJiraIssues.count_documents({})
+
+        skip = (page - 1) * size
+        cursor = collectionJiraIssues.find().skip(skip).limit(size)
+
+        issues = list(cursor)
+        for issue in issues:
+            issue["_id"] = str(issue["_id"])
+        if issues:
+            total_items = collectionJiraIssues.count_documents({})
+            total_pages = (total_items + size - 1) // size
+            res = {
+                "issues": issues,
+                "currentPage": page,
+                "totalItems": total_items,
+                "totalPages": total_pages
+            }
+        else:
+            res = {
+                "issues": issues,
+                "currentPage": page,
+                "totalItems": 0,
+                "totalPages": 0
+            }
+
+        return jsonify(res), 200
+    except Exception as e:
+        return jsonify({"error": "Internal Server Error"}), 500
+
+
+@jira_issue_bp.route("/projectNames", methods=["GET"])
+def get_all_project_names():
+    project_names = list(collectionJiraProjects.find({}))
+    for element in project_names:
+        element["_id"] = str(element["_id"])
+    return project_names
+
+
+@jira_issue_bp.route("/load/issueTypes/<project_name>")
+def load_issue_types_from_jira_issues(project_name):
+    issue_types = []
+
+    try:
+        base_url = "https://jira-se.ifi.uni-heidelberg.de"
+        uri = f"{base_url}/rest/api/2/search?jql=project={project_name}&maxResults=10000"
+
+        response = requests.get(
+            uri,
+            auth=(os.environ['username'], os.environ['password']),
+            headers={"Accept": "application/json"}
+        )
+        response_json = response.json()
+        set_new_project_names(response_json["issues"][0]["fields"]["project"]["name"])
+        total_issues = int(response_json.get("total", 0))
+
+        for i in range(total_issues):
+            issue_type = response_json["issues"][i]["fields"]["issuetype"]["name"]
+            if issue_type not in issue_types:
+                issue_types.append(issue_type)
+    except Exception as e:
+        pass
+
+    return issue_types
+
+
+def set_new_project_names(project_name):
+    filter_query = {"name": project_name}
+    # matching_elements = list(collectionJiraProjects.find(filter_query))
+
+    if list(collectionJiraProjects.find(filter_query)):
+        return jsonify({"message": "Einträge mit dem Namen 'test' gefunden."})
+    else:
+        collectionJiraProjects.insert_one(filter_query)
