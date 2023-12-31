d30f2d437713af4a8dd70c7659034ea30125dfed
tunao <benjamintuna@gmail.com>
2023-12-31 16:23:18 +0100

    adjustments after static tests

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
 from controller.feedback import feedback_bp
 from controller.jira import jira_issue_bp
 from controller.issue_feedback_relation import issue_feedback_relation_bp
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.register_blueprint(feedback_bp, url_prefix='/hitec/jira/feedback')
 app.register_blueprint(jira_issue_bp, url_prefix='/hitec/jira/issues')
 app.register_blueprint(issue_feedback_relation_bp, url_prefix='/hitec/jira/issue_feedback')
 
 if __name__ == '__main__':
-    app.run(port=9647)
+    app.run(host='0.0.0.0', port=9647)
