0f138de2f93cc9b624440d1929d0cc6ab44512c3
tunao <benjamintuna@gmail.com>
2023-12-31 16:20:03 +0100

    adjustments after static tests

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from controller.jira import jira_issue_bp
 from controller.issue_feedback_relation import issue_feedback_relation_bp
 
 app = Flask(__name__)
+
 app.register_blueprint(feedback_bp, url_prefix='/hitec/jira/feedback')
 app.register_blueprint(jira_issue_bp, url_prefix='/hitec/jira/issues')
 app.register_blueprint(issue_feedback_relation_bp, url_prefix='/hitec/jira/issue_feedback')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9647)
+    app.run(port=9647)
