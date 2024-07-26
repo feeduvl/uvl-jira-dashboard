52d21aa591cb44d4610cf5458d228ea960c3d9ed
timmmerlan <timm.merlan@web.de>
2024-07-26 10:41:03 +0200

    UVL Dashboard - transfer feedback datasets in request body, json format

--- controller/issue_feedback_relation.py
+++ controller/issue_feedback_relation.py
@@ -265,7 +265,7 @@ def assign_feedback_to_issues(feedback_name, max_similarity_value):
 
 @issue_feedback_relation_bp.route('/assign_many_feedback_to_issues/<max_similarity_value>', methods=['POST'])
 def assign_many_feedback_to_issues(max_similarity_value):
-    feedback_list = request.form.get('datasets')
+    feedback_list = request.json.get('datasets')
     print(feedback_list)
     max_similarity_value = float(max_similarity_value)
     # delete all assignments to create new
