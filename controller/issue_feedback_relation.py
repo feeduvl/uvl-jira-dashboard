723ded609901ed9415cfc5866526d057a2fe6eac
timmmerlan <timm.merlan@web.de>
2024-07-27 11:14:24 +0200

    UVL Dashboard - logging

--- controller/issue_feedback_relation.py
+++ controller/issue_feedback_relation.py
@@ -316,6 +316,7 @@ def assign_many_feedback_to_issues(max_similarity_value):
 def calculate_feedback_embedding(feedback_name):
     logging.error(feedback_name)
     feedback_document = collection_feedback.find_one({"name": feedback_name})
+    logging.error("feedback_document")
     logging.error(feedback_document)
     feedback_array = feedback_document.get("feedback", [])
     feedback_embeddings = []
