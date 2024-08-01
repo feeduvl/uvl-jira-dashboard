064ddcf7cb454bfb3186b5fb9bbf95d60b1593f5
timmmerlan <timm.merlan@web.de>
2024-08-01 12:01:36 +0200

    UVL Dashboard - logging

--- controller/issue_feedback_relation.py
+++ controller/issue_feedback_relation.py
@@ -316,7 +316,7 @@ def assign_many_feedback_to_issues(max_similarity_value):
 
 def calculate_feedback_embedding(feedback_name):
     logging.error(feedback_name)
-    feedback_document = collection_imported_feedback.find_one({"dataset": feedback_name})
+    feedback_document = collection_imported_feedback.find_one({"name": feedback_name})
     #feedback_document = collection_feedback.find_one({"name": feedback_name}) #TODO: Ursprungszustand wiederherstellen und log anschauen
     logging.error("feedback_document")
     logging.error(feedback_document)
