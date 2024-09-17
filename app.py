f869c1a3785177de580cbe6f96527f57394853b5
timmmerlan <timm.merlan@web.de>
2024-09-17 09:55:22 +0200

    UVL Dashboard - own class for dashboard

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.register_blueprint(feedback_bp, url_prefix='/hitec/jira/feedback')
 app.register_blueprint(jira_issue_bp, url_prefix='/hitec/jira/issues')
 app.register_blueprint(issue_feedback_relation_bp, url_prefix='/hitec/jira/issue_feedback')
-app.register_blueprint(dashboard_bp, url_prefix='/hitec/uvldashboard')
+app.register_blueprint(dashboard_bp, url_prefix='/hitec/jira/uvldashboard')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9647)
